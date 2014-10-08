fda2e75fcbfa8ac6ccb35172a1b60d36beff9198
Keisuke Uozumi <eroejp@gmail.com>
2014-10-08 10:38:33 +0900

    add BrewInstall

---
+++ BrewInstall.sh
@@ -0,0 +1,44 @@
+# Make sure using latest Homebrew
+brew update
+
+# Update already-installed formula
+brew upgrade
+
+# Add Repository
+brew tap phinze/homebrew-cask || true
+brew tap homebrew/dupes || true
+brew tap homebrew/binary || true
+
+# Packages for development
+brew install git
+brew install homebrew/dupes/grep
+brew install hub
+brew install markdown
+brew install mosh
+brew install tmux
+brew install tree
+brew install vim
+brew install wget
+brew install zsh
+brew install zsh-completions
+
+# Packages for brew-cask
+brew install brew-cask
+
+# .dmg from brew-cask
+brew cask install atom
+brew cask install boot2docker
+brew cask install dropbox
+brew cask install google-chrome
+brew cask install google-japanese-ime
+brew cask install InsomniaX
+brew cask install IPinmenubar
+brew cask install iterm2
+brew cask install kobito
+brew cask install shiftit
+brew cask install virtualbox
+brew cask install xtrafinder
+brew cask install xquartz
+
+# Remove outdated versions
+brew cleanup
