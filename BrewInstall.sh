d9a9784fbb9f6c9f95cba4eda577935fbd55144d
kuozumijp <eroejp@gmail.com>
2015-03-13 16:39:45 +0900

    Update BrewInstall.sh

--- BrewInstall.sh
+++ BrewInstall.sh
@@ -51,6 +51,7 @@ brew cask install ip-in-menu-bar
 brew cask install iterm2
 brew cask install java
 brew cask install kindle
+brew cask install kitematic
 brew cask install macdown
 brew cask install mysqlworkbench
 brew cask install shiftit
