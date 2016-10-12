18ea4d45db7e1ef065f7d1ddbd610f5b8c351db9
Keisuke Uozumi <eroejp@gmail.com>
2016-10-12 13:13:49 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@
 # install.sh
 #
 
+mkdir -p ~/.zsh/completions
 cp -r ./bin ~/
 
 cp ./.*rc ~/
@@ -12,5 +13,8 @@ cp -r ./.vim ~/
 cp -r ./.tmux ~/
 cp ./.gitconfig ~/
 cp ./.tmux.conf ~/
+
+curl -L https://raw.githubusercontent.com/felixr/docker-zsh-completion/master/_docker > ~/.zsh/completions/_docker
+curl -L https://raw.githubusercontent.com/github/hub/master/etc/hub.zsh_completion > ~/.zsh/completions/_hub
 
 curl -L git.io/nodebrew | perl - setup
