c97dfc858563d435a2d4324f2b753043f8bf5567
Keisuke Uozumi <eroejp@gmail.com>
2016-10-28 15:21:59 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -16,5 +16,6 @@ cp ./.tmux.conf ~/
 
 curl -L https://raw.githubusercontent.com/felixr/docker-zsh-completion/master/_docker > ~/.zsh/completions/_docker
 curl -L https://raw.githubusercontent.com/github/hub/master/etc/hub.zsh_completion > ~/.zsh/completions/_hub
+curl -L https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/plugins/knife/_knife > ~/.zsh/completions/_knife
 
 curl -L git.io/nodebrew | perl - setup
