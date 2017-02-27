a8b3b785bad4a42ee8724aa0b47a2bc59820b080
Keisuke Uozumi <eroejp@gmail.com>
2017-02-27 19:31:56 +0900

    Update install.sh

--- install.sh
+++ install.sh
@@ -29,6 +29,8 @@ if [ "$(uname)" == 'Darwin' ]; then
   /usr/bin/ruby -e "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/master/install)"
 elif [ "$(expr substr $(uname -s) 1 5)" == 'Linux' ]; then
   cp ./.zshrc.local.rhel ~/.zshrc.local
+  # zshcompletions install
+  git clone git://github.com/zsh-users/zsh-completions.git ~/.zsh/zsh-completions
   # vvm - vim version manager
   curl https://raw.githubusercontent.com/kana/vim-version-manager/master/bin/vvm | python - setup
   # go
