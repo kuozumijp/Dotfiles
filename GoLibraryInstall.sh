101d4bcb9bc0e4013605a61201454227ce9d935f
Keisuke Uozumi <197764+kuozumijp@users.noreply.github.com>
2017-11-16 11:29:22 +0900

    Update GoLibraryInstall.sh

--- GoLibraryInstall.sh
+++ GoLibraryInstall.sh
@@ -1,8 +1,2 @@
 go get github.com/motemen/ghq
 go get github.com/github/hub
-#go get github.com/peco/peco/cmd/peco
-wget 'https://github.com/peco/peco/releases/download/v0.4.7/peco_linux_amd64.tar.gz'
-tar zxvf peco_linux_amd64.tar.gz
-mv peco_linux_amd64/peco ~/.go/bin/
-rm peco_linux_amd64.tar.gz
-rm -r peco_linux_amd64
