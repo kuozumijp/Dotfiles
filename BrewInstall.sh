5d3d021392701b7c597e9b6a7808c0e8840b5ab1
Keisuke Uozumi <197764+kuozumijp@users.noreply.github.com>
2019-01-31 16:22:48 +0900

    Update BrewInstall.sh

--- BrewInstall.sh
+++ BrewInstall.sh
@@ -119,7 +119,7 @@ brew cask install shiftit
 brew cask install caskroom/homebrew-cask/sqlitebrowser
 brew cask install squidman --force --no-quarantine
 brew cask install totalspaces
-brew cask install trailer
+brew cask install trailer --force --no-quarantine
 brew cask install tunnelblick
 brew cask install visual-studio-code --force --no-quarantine
 brew cask install vlc
